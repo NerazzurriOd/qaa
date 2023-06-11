5d7125b3d337c4b6a76e0d447119e3cfbc68d88b
mdynik <dyniknikolay@gmail.com>
2023-06-11 14:33:28 +0300

    add locators file for appliances

--- hw_20/pages/category_kitchen_technique.py
+++ hw_20/pages/category_kitchen_technique.py
@@ -1,12 +1,13 @@
 from hw_20.pages.basepage import BasePage
 from hw_20.pages.electric_grill_page import ElectricGrillPage
 from hw_20.core.locator import Locator
+from hw_20.locators.appliances_locators import AppliancesLocators
 
 
 class CategoryPickPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.__for_kitchen_technique_locator = Locator('xpath', '//a[@title="Для кухні"]')
+        self.__for_kitchen_technique_locator = AppliancesLocators().for_kitchen_technique_locator
 
     def choose_kitchen_technique_category(self):
         self._click(self.__for_kitchen_technique_locator)
